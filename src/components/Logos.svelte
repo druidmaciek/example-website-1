4b093ca277f94081687e20f74741d7ee2187d492
druidmaciek <maciej@janowski.dev>
2021-05-09 09:19:03 +0200

    update logo cloud

--- src/components/Logos.svelte
+++ src/components/Logos.svelte
@@ -1,8 +1,8 @@
-<div class="my-24 bg-white">
-	<p class="text-center text-sm font-semibold uppercase text-gray-500 tracking-wide">
-		Some of our trusted customers
-	</p>
+<div class="mb-12 bg-white">
 	<div class="mt-12 max-w-7xl mx-auto pt-4  px-4 sm:px-6 lg:px-8">
+		<p class="text-center text-sm font-semibold uppercase text-gray-500 tracking-wide">
+			Some of our trusted customers
+		</p>
 		<div class="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
 			<div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
 				<img
